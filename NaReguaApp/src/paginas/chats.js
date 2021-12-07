3b70589d20691ee94cc4114bfaa19b9d9e2740e0
luna <eduardaluna22@gmail.com>
2021-12-07 13:52:53 -0300

    footer de prestador na tela de chats

--- NaReguaApp/src/paginas/chats.js
+++ NaReguaApp/src/paginas/chats.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 
 const Chats = ({ navigation })  => {
-    let prestador = false;
+    let prestador = true;
     let dados = [
         {
             nome: 'Nome',
@@ -76,6 +76,29 @@ const Chats = ({ navigation })  => {
         <TouchableOpacity style={styles.appFooterIcon} onPress={() => navigation.navigate('PerfilCliente')}>
           <Icon name="user" type="FontAwesome" size={24} color="grey"/>
           <Text style={styles.appFooterText}>Perfil</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+    }
+    {prestador == true &&
+    <View style={styles.appFooter}>
+      <Divider style={styles.divider}></Divider>
+      <View style={styles.appFooterRow}>
+        <TouchableOpacity style={styles.appFooterIcon} onPress={() => navigation.navigate('InicioPrestador')}>
+          <Icon name="home" type="FontAwesome" size={27} color="grey"/>
+          <Text style={styles.appFooterText}>Início</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.appFooterIcon} onPress={() => ''}>
+          <Icon name="store" type="MaterialIcons" size={27} color="grey"/>
+          <Text style={styles.appFooterText}>Minha Loja</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.appFooterIcon} onPress={() => ''}>
+          <Icon name="dollar" type="FontAwesome" size={23} color="grey"/>
+          <Text style={styles.appFooterText}>Histórico</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.appFooterIcon} onPress={() => ''}>
+          <Icon name="user" type="FontAwesome" size={24} color="black"/>
+          <Text style={styles.appFooterTextActive}>Perfil</Text>
         </TouchableOpacity>
       </View>
     </View>
